3dd7bda11507d0be5cd9c5299ac9d696cee0b3e3
zaklil <114111271+zaklil@users.noreply.github.com>
2022-09-22 12:34:05 +0300

    Update Program-for-finding-volume-and-temperature-in-a-bowl.cpp

--- Program-for-finding-volume-and-temperature-in-a-bowl.cpp
+++ Program-for-finding-volume-and-temperature-in-a-bowl.cpp
@@ -1,37 +1,29 @@
- //Написати програму для знаходження обсягу і температури в посудині.
 #include <iostream>
 using namespace std;
  int main() 
 {
-	//Оголошуєм змінні
-	int v1;
+
+    int v1;
     int v2;
     int v3;
     int t1;
     int t2;
     int t3;
-	//Виводимо повідомлення для користувача
-	cout <<"Vvedit chisla dla rozrahynky ";
-	//Заносимо дані які ввели
+	
+  cout <<"enter the numbers for the calculation and write the start ";
 	cin >> v1;
 	cin >> v2;
 	cin >> v3;
 	cin >> t1;
 	cin >> t2;
 	cin >> t3;
-    //Заносимо дані які ввів користувач в змінну
     cin>> t1;
     cin>> v1;
     v3=v1+v2;
     t3=(t1+t2)/2;
-    //Виводимо результат праграми
-    cout<<"Obem stanovut"<<v1<<endl;
-    cout<<"Temperatyra stanovut="<<t1<<endl;
+    
+    cout<<"the volume is"<<v1<<endl;
+    cout<<"the temperature is="<<t1<<endl;
     system("pause");
 	return 0;
 }
-
-
-
-
-
